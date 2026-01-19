faac3698892556fe103cc53b9b3984db54ec16e4
이혜연 <lehy1124@naver.com>
2026-01-19 14:47:56 +0900

    feat : 운동 별 사용 관절 조회 함수 추가

--- backend-iot/src/main/java/com/example/iot/repository/ExerciseJointMappingRepository.java
+++ backend-iot/src/main/java/com/example/iot/repository/ExerciseJointMappingRepository.java
@@ -1,7 +1,16 @@
 package com.example.iot.repository;
 
 import com.example.iot.domain.ExerciseJointMapping;
+import com.example.iot.domain.ExerciseSensorMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExerciseJointMappingRepository extends JpaRepository<ExerciseJointMapping, Long> {
+
+    //운동 별 필요한 Joint 조회
+    List<ExerciseJointMapping> findByExercise_Id(Long exerciseId);
+    //운동 별 필요한 Joint가 있는지 확인
+    boolean existsByExercise_IdAndJoint_Id(Long exerciseId, String jointId);
+
 }
