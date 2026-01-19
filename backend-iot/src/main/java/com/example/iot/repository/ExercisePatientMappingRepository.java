a362fcd5b3cb112744da474ceac4d78e7c106bec
이혜연 <lehy1124@naver.com>
2026-01-19 14:48:23 +0900

    feat: 환자별 필요한 운동 조회 함수 추가

--- backend-iot/src/main/java/com/example/iot/repository/ExercisePatientMappingRepository.java
+++ backend-iot/src/main/java/com/example/iot/repository/ExercisePatientMappingRepository.java
@@ -3,5 +3,12 @@ package com.example.iot.repository;
 import com.example.iot.domain.ExercisePatientMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExercisePatientMappingRepository extends JpaRepository<ExercisePatientMapping, Long> {
+
+    //환자 id에 할당된 현재 운동 조회
+    List<ExercisePatientMapping> findByPatient_Id(Long patientId);
+    //현재 해야하는 운동 있는지 판단
+    boolean existsByExercise_IdAndPatient_Id(Long exerciseId, Long patientId);
 }
