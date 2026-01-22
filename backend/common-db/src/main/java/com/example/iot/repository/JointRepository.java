c83f55528f9c31fc291ec27126cdf0a81e2e4cc8
신원호 <swh4077@naver.com>
2026-01-22 16:35:21 +0900

    fix: repository 파일의 타입 오기입 수정

--- backend/common-db/src/main/java/com/example/iot/repository/JointRepository.java
+++ backend/common-db/src/main/java/com/example/iot/repository/JointRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JointRepository extends JpaRepository<Joint, Long> {
+public interface JointRepository extends JpaRepository<Joint, String> {
 }
