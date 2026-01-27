a7a32d0095966111eccb269754dba5d11130f763
이혜연 <lehy1124@naver.com>
2026-01-27 16:38:22 +0900

    feat: redis ê집계값 받아오기

--- backend/backend-iot/src/main/java/com/example/iot/controller/TryController.java
+++ backend/backend-iot/src/main/java/com/example/iot/controller/TryController.java
@@ -2,9 +2,11 @@ package com.example.iot.controller;
 
 import com.example.iot.dto.response.TryFinishResponse;
 import com.example.iot.dto.response.TryStartResponse;
+import com.example.iot.service.IngestClient;
 import com.example.iot.service.IngestRedisService;
 import com.example.iot.service.TryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,20 +14,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/tries")
-@RequiredArgsConstructor // 생성자 주입 자동 생성
 public class TryController {
 
+    private final IngestClient ingestClient;
     private final TryService tryService;
     private final IngestRedisService redisService; // Redis 조회용
 
     public TryController(TryService tryService,
                          IngestRedisService redisService,
-                         StringRedisTemplate redisTemplate
+                         IngestClient ingestClient
                          ) {
         this.redisService = redisService;
         this.tryService = tryService;
+        this.ingestClient = ingestClient;
     }
     /**
      * 운동 시도 종료
@@ -35,8 +39,14 @@ public class TryController {
     public ResponseEntity<TryFinishResponse> finish(@PathVariable Long tryId) {
         // 1. Redis에서 해당 Try의 프레임 데이터 리스트 조회
         // 키 패턴은 "frames:try:{tryId}"로 가정
+        //일단 있는 값 다 가져오기
         String rawFrames = redisService.getLatest();
-
+        Map<Object, Object> rawAgg = redisService.getAgg(tryId);
+        //로그로 최종확인
+        log.info("rawAgg = {}", (String) rawAgg.getOrDefault("sum.strength", "0"));
+        log.info("request = {}", rawFrames);
+        //끝났음 신호
+        ingestClient.stopTry();
         // 2. 조회된 데이터를 포함하여 서비스 호출
         return ResponseEntity.ok(tryService.finishTry(tryId, rawFrames));
     }
@@ -46,6 +56,7 @@ public class TryController {
      */
     @PostMapping("/{tryId}/start")
     public ResponseEntity<TryStartResponse> start(@PathVariable Long tryId) {
+        ingestClient.startTry(String.valueOf(tryId));
         return ResponseEntity.ok(tryService.startTry(tryId));
     }
 
--- backend/backend-iot/src/main/java/com/example/iot/service/IngestRedisService.java
+++ backend/backend-iot/src/main/java/com/example/iot/service/IngestRedisService.java
@@ -3,6 +3,8 @@ package com.example.iot.service;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class IngestRedisService {
 
@@ -14,5 +16,10 @@ public class IngestRedisService {
 
     public String getLatest() {
         return redisTemplate.opsForValue().get("ingest:latest");
+    }
+
+    public Map<Object, Object> getAgg(Long tryId) {
+        String key = "agg:try:" + tryId;
+        return redisTemplate.opsForHash().entries(key);
     }
 }
