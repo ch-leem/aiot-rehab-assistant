a69ed05721cf3fa504df0154cbc30379d01bb2f6
이혜연 <lehy1124@naver.com>
2026-02-04 16:25:53 +0900

    feat: download -> response

--- backend/backend-user/src/main/java/com/example/user/controller/TryDebugController.java
+++ backend/backend-user/src/main/java/com/example/user/controller/TryDebugController.java
@@ -15,28 +15,40 @@ public class TryDebugController {
 
     private final RestTemplate restTemplate;
 
-    @GetMapping("/{tryId}/fail-log-file")
-    public ResponseEntity<byte[]> getFailLog(@PathVariable Long tryId) {
+//    @GetMapping("/{tryId}/fail-log-file")
+//    public ResponseEntity<byte[]> getFailLog(@PathVariable Long tryId) {
+//        String url = "http://iot-api:8080/tries/" + tryId + "/fail-log-file";
+//
+//        ResponseEntity<byte[]> resp = restTemplate.exchange(
+//                url,
+//                HttpMethod.GET,
+//                new HttpEntity<>(new HttpHeaders()),
+//                byte[].class
+//        );
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        // iot 서버가 주는 content-type을 최대한 유지
+//        MediaType ct = resp.getHeaders().getContentType();
+//        if (ct != null) headers.setContentType(ct);
+//
+//        // 파일 다운로드처럼 보이게 하고 싶으면(선택)
+//        String disposition = resp.getHeaders().getFirst(HttpHeaders.CONTENT_DISPOSITION);
+//        if (disposition != null) headers.set(HttpHeaders.CONTENT_DISPOSITION, disposition);
+//
+//        return new ResponseEntity<>(resp.getBody(), headers, resp.getStatusCode());
+//    }
+
+    @GetMapping(value = "/{tryId}/fail-log-file", produces = "application/x-ndjson")
+    public ResponseEntity<byte[]> getFailLogAsNdjson(@PathVariable Long tryId) {
         String url = "http://iot-api:8080/tries/" + tryId + "/fail-log-file";
 
-        ResponseEntity<byte[]> resp = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                new HttpEntity<>(new HttpHeaders()),
-                byte[].class
-        );
+        ResponseEntity<byte[]> resp = restTemplate.exchange(url, HttpMethod.GET, null, byte[].class);
 
         HttpHeaders headers = new HttpHeaders();
-        // iot 서버가 주는 content-type을 최대한 유지
-        MediaType ct = resp.getHeaders().getContentType();
-        if (ct != null) headers.setContentType(ct);
-
-        // 파일 다운로드처럼 보이게 하고 싶으면(선택)
-        String disposition = resp.getHeaders().getFirst(HttpHeaders.CONTENT_DISPOSITION);
-        if (disposition != null) headers.set(HttpHeaders.CONTENT_DISPOSITION, disposition);
-
+        headers.setContentType(MediaType.parseMediaType("application/x-ndjson; charset=utf-8"));
         return new ResponseEntity<>(resp.getBody(), headers, resp.getStatusCode());
     }
+
 
 
 }
